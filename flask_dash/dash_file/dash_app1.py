9d91f1257c56cd5ced6e909a16a523247cdf2efd
AraRen <jackyccz860511@gmail.com>
2023-12-22 01:10:10 +0000

    mod

--- flask_dash/dash_file/dash_app1.py
+++ flask_dash/dash_file/dash_app1.py
@@ -1,22 +1,27 @@
 import pandas as pd
-from dash import Dash, dcc, html, Input, Output, State, callback
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
 dash1 = Dash(requests_pathname_prefix="/dash/app1/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-dash1.title = "Youbike站點車輛訊息"
+dash1.title = "大安捷運站出口-Youbike站點車輛訊息"
 
 dash1.layout = html.Div([
-    html.H1('捷運大安站Youbike站點車輛訊息'),
+    html.H1('        Youbike站點車輛訊息'),
     dcc.Graph(id='graph'),
+    
 ])
-   
+
 @dash1.callback(
     Output("graph", "figure"),
     Input("graph", "id")
 )
 def update_line_chart(station):
-    df = pd.read_csv('./dash_file/Youbike1221_2.csv')
-    fig = px.line(df, x="status", y="number", color='station')
+    df = pd.read_csv('./dash_file/Youbike1221_4.csv')
+    fig = px.line(df, x="status", y="number", color='station',text='number')
+    fig.update_traces(textposition="bottom right")
+
     return fig
+
+
